ff17c1b5b86154530f397f6d65b3da458aa63e71
Hai Chang <haichang@microsoft.com>
2021-03-22 15:07:43 +0800

    Push image to buildout acr

--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker image build -t beacr.azurecr.io/cleanup-docker .
+docker image build -t beacr.azurecr.io/cleanup-docker -t buildout.azurecr.io/cleanup-docker .
 docker push beacr.azurecr.io/cleanup-docker
+docker push buildout.azurecr.io/cleanup-docker
