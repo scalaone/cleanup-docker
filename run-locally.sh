61c7c9112e49a6d819fd6498cda05a7d44bcdd7d
Hai Chang <haichang@microsoft.com>
2022-02-09 22:42:27 +0800

    Push to acr

--- run-locally.sh
+++ run-locally.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker image build -t beacr.azurecr.io/cleanup-docker -t buildout.azurecr.io/cleanup-docker .
-docker push beacr.azurecr.io/cleanup-docker
+docker image build -t bettest.azurecr.io/cleanup-docker -t buildout.azurecr.io/cleanup-docker -t betprod.azurecr.io/cleanup-docker .
+docker push bettest.azurecr.io/cleanup-docker
+docker push betprod.azurecr.io/cleanup-docker
 docker push buildout.azurecr.io/cleanup-docker
